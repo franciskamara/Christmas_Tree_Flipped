68343ee5e18fa5e6a52d8fb69ca8bd8e06f534e5
franciskamara <53877017+franciskamara@users.noreply.github.com>
2023-12-20 10:35:10 +0000

    Add files via upload

---
+++ Christmas_Tree_Flipped/Christmas_Tree_Flipped/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Christmas_Tree_Flipped // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Christmas Tree upside down");
+
+            Console.Write("How many lines do you want to see: ");
+            int totalRows = int.Parse(Console.ReadLine());
+            
+            Console.Clear();
+            
+            
+            for (int rowCount = 0; rowCount <= totalRows; rowCount++)
+            {
+                for (int spaceCount = 0; spaceCount < rowCount; spaceCount++)
+                {
+                    Console.Write("  ");
+                }
+                
+                for (int starCount = 0; starCount < (totalRows - rowCount) *2-1 ; starCount++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
